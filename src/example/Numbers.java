bbc430461e5bab21401e40d7b6468eecd916abf4
User <arturartur698@gmail.com>
2020-11-16 22:33:56 +0200

    Commit 2

---
+++ src/example/Numbers.java
@@ -0,0 +1,29 @@
+package example;
+
+public class Numbers {
+    // x+3=5
+
+    static byte a = 126;
+    static short b = 777;
+    static int c = 35_000;
+    static long d = 1_000_000_000_000_000L;
+
+    static char e = 'C';
+
+    static float f = 12345678910111213f;
+    static double g = 6.3;
+
+    static boolean h = true;
+
+
+    public static void main(String[] args) {
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+}
