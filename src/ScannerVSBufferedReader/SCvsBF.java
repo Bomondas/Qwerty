934d516c742c9dbcfd17807ffcac5382520b9558
User <arturartur698@gmail.com>
2021-02-17 22:46:15 +0200

    Commit 2

---
+++ src/ScannerVSBufferedReader/SCvsBF.java
@@ -0,0 +1,13 @@
+package ScannerVSBufferedReader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class SCvsBF {
+    public static void main(String[] args) throws IOException {
+
+
+    }
+}
--- src/example/HelloWorld.java
+++ src/example/HelloWorld.java
@@ -1,8 +1,22 @@
 package example;
 
+
+import java.io.CharArrayWriter;
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Line");
+        // Арифметичсекие операции над значениями типа double
+        System.out.println("Арифметика с плавающей точкой");
+        double da = 1 + 1;
+        double db = da * 3;
+        double dc = db / 4;
+        double dd = dc - da;
+        double de = - dd;
+        System.out.println("da " +da);
+        System.out.println("db " +db);
+        System.out.println("dc " +dc);
+        System.out.println("dd " +dd);
+        System.out.println("de " +de);
     }
 }
